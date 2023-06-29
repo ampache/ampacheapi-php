fb71d3b74f4f169fc3c09a5b13c4a0141a6f4960
lachlan <lachlan.00@gmail.com>
2023-06-29 10:03:55 +1000

    Create .php-cs-fixer.php

---
+++ .php-cs-fixer.php
@@ -0,0 +1,44 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->exclude(['vendor', 'nbproject', '.idea'])
+    ->in('src/')
+;
+
+$config = new PhpCsFixer\Config();
+return $config->setRules([
+        '@PSR2' => true,
+        'binary_operator_spaces' => [
+            'operators' => ['=' => 'align']
+        ],
+        'blank_line_after_namespace' => true,
+        'blank_line_before_statement' => [
+            'statements' => ['declare', 'return']
+        ],
+        'concat_space' => [
+            'spacing' => 'one'
+        ],
+        'curly_braces_position' => [
+            'functions_opening_brace' => 'next_line_unless_newline_at_signature_end',
+            'allow_single_line_empty_anonymous_classes' => true,
+            'allow_single_line_anonymous_functions' => true
+        ],
+        'constant_case' => true,
+        'elseif' => true,
+        'encoding' => true,
+        'full_opening_tag' => true,
+        'line_ending' => true,
+        'method_argument_space' => false,
+        'no_break_comment' => false,
+        'no_trailing_whitespace' => true,
+        'no_whitespace_in_blank_line' => true,
+        'single_blank_line_at_eof' => true,
+        'strict_param' => false,
+        'visibility_required' => false,
+    ])
+    ->setIndent("    ")
+    ->setUsingCache(false)
+    ->setFinder($finder)
+    ->setLineEnding("\n")
+;
+
